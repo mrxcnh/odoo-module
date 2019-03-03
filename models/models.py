7b2c2537fa16215018e0fc6296d5fec369ce5eee
thanhdo <thanhdo@tamdongtam.vn>
2019-03-03 10:03:55 +0700

    [ADD] add state to models and header for form in views

--- models/models.py
+++ models/models.py
@@ -11,3 +11,30 @@ class CarRequest(models.Model):
     date_to = fields.Datetime(string="End Date", required=False, )
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
     car_id = fields.Many2one(comodel_name="fleet.vehicle", string="Car", required=True, )
+    state = fields.Selection(
+        string="Status",
+        selection=[
+            ('draft', 'Draft'),
+            ('confirm', 'Confirm'),
+            ('validate', 'Validated'),
+            ('refuse', 'Refuse'),
+            ('approve', 'Approved'),
+        ],
+        default="draft",
+    )
+
+    @api.multi
+    def confirm_request(self):
+        self.state = 'confirm'
+
+    @api.multi
+    def validate_request(self):
+        self.state = 'validate'
+
+    @api.multi
+    def refuse_request(self):
+        self.state = 'refuse'
+
+    @api.multi
+    def approve_request(self):
+        self.state = 'approve'
